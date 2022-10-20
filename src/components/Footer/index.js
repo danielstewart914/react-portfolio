268a754aa09d79951622d53db7c05f76a2ddf443
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-10-20 12:58:21 -0700

    Added Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import styles from  './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.Footer}>
+            <address className={styles.Address}>
+                <a href='https://www.linkedin.com/in/daniel-stewart-230b1623a/' target='_blank' rel='noreferrer'>
+                    <img src='images/In-blue-logo.png' alt='LinkedIn Icon' className={styles.Icon} />&nbsp;LinkedIn
+                </a>
+                <a href='https://github.com/danielstewart914'>
+                    <img src='images/GitHub-Mark-120px-plus.webp' alt='GitHub Icon' className={styles.Icon} />&nbsp;GitHub 
+                </a>
+                <a href='mailto:danielstewart914@outlook.com'>
+                    <img src='images/envelope.svg' alt='Email Icon' className={styles.Icon} />&nbsp;danielstewart914@outlook.com
+                </a>
+            </address>
+        </footer>
+    )
+};
+
+export default Footer;
