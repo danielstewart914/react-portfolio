1eba66ba4160f168dc18e1baeaa1a3c0f1e6fe6f
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2023-03-20 14:45:55 -0700

    Update Contact form to connect with Formspree

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,126 +1,133 @@
 import { useState } from "react";
 import styles from './ContactForm.module.css';
+import { useForm } from '@formspree/react';
 
 const ContactForm = () => {
-    // state variables
-    const [ name, setName ] = useState('');
-    const [ email, setEmail ] = useState('');
-    const [ message, setMessage ] = useState('');
-    const [ errorMessage, setErrorMessage ] = useState('');
+  // state variables
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ message, setMessage ] = useState('');
+  const [ errorMessage, setErrorMessage ] = useState('');
+  const [state, handleSubmit] = useForm('xdovlenb');
 
-    // methods for setting input
-    const set = {
-        name: value => setName( value ),
-        email: value => setEmail( value ),
-        message: value => setMessage( value )
+  // methods for setting input
+  const set = {
+    name: value => setName(value),
+    email: value => setEmail(value),
+    message: value => setMessage(value)
+  }
+
+  // error messages
+  const displayError = {
+    name: 'You must enter a name!',
+    email: 'Email is not valid!',
+    message: 'You must enter a message!'
+  }
+
+  // test methods for input elements
+  const check = {
+    name: value => {
+      return !!value.length;
+    },
+    email: value => {
+      return /^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(value);
+    },
+    message: value => {
+      return !!value.length;
+    }
+  }
+
+  // check input when input loses focus
+  const handleFocusOut = ( event ) => {
+    const { name: inputField, value } = event.target;
+    if (!check[inputField](value)) setErrorMessage( displayError[inputField]);
+  }
+
+  // set input on change
+  const handleInputChange = ( event ) => {
+    const { name: inputField, value } = event.target;
+    setErrorMessage('');
+    return set[inputField](value);
+  }
+
+  // function for form submission
+  const handleFormSubmit = ( event ) => {
+    event.preventDefault();
+    if ( !check.name(name) ) {
+      setErrorMessage(displayError['name']);  
+      return;
+    } 
+    if ( !check.email(email) ) {
+      setErrorMessage(displayError['email']);
+      return;
+    }
+    if ( !check.message(message) ) {
+      setErrorMessage(displayError['message']);
+      return;
     }
 
-    // error messages
-    const displayError = {
-        name: 'You must enter a name!',
-        email: 'Email is not valid!',
-        message: 'You must enter a message!'
-    }
+    handleSubmit(event);
 
-    // test methods for input elements
-    const check = {
-        name: value => {
-            return value.length ? true : false;
-        },
-        email: value => {
-            return /^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test( value );
-        },
-        message: value => {
-            return value.length ? true : false;
-        }
-    }
+    // reset inputs
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
 
-    // check input when input loses focus
-    const handleFocusOut = ( event ) => {
-        const { name: inputField, value } = event.target;
-        if ( !check[ inputField ]( value ) ) setErrorMessage( displayError[ inputField ] );
-    }
+  if (state.succeeded) {
+      return <p className={styles.Statement}>Thanks for your Submission!</p>
+  }
 
-    // set input on change
-    const handleInputChange = ( event ) => {
-        const { name: inputField, value } = event.target;
-        setErrorMessage('');
-        return set[ inputField ]( value );
-    }
-
-    // function for form submition
-    const handleFormSubmit = ( event ) => {
-        event.preventDefault();
-        if ( !check.name( name ) ) {
-            setErrorMessage( displayError[ 'name' ] );  
-            return;
-        } 
-        if ( !check.email( email ) ) {
-            setErrorMessage( displayError[ 'email' ] );
-            return;
-        }
-        if ( !check.message( message ) ) {
-            setErrorMessage( displayError[ 'message' ] );
-            return;
-        }
-
-        // add code to submit code to backend server
-
-        // reset inputs
-        setName('');
-        setEmail('');
-        setMessage('');
-        setErrorMessage( 'Message has been sent!' )
-    }
-
-    return (
-        <form>
-            <label >
-                Name:
-                <input
-                    value={name}
-                    name='name'
-                    onChange={handleInputChange}
-                    onBlur={handleFocusOut}
-                    type='text'
-                    placeholder='Name'
-                />
-            </label>
-            <label>
-                Email:
-                <input
-                    value={email}
-                    name='email'
-                    onChange={handleInputChange}
-                    onBlur={handleFocusOut}
-                    type='text'
-                    placeholder='Email'
-                />
-            </label>
-            <label>
-                Message:
-                <textarea
-                    value={message}
-                    name='message'
-                    onChange={handleInputChange}
-                    onBlur={handleFocusOut}
-                    type='text'
-                    placeholder='Message'
-                    rows={10}
-                />
-            </label>
-            <div className={styles.Footer}>
-                <button 
-                    className='Button'
-                    onClick={handleFormSubmit}
-                >
-                    Submit
-                </button>
-                <span className={styles.Message}>{errorMessage}</span>
-            </div>
-        </form>
-
-    )
+  return (
+    <>
+      <p className={styles.Statement}>Please fill out the form below to contact me, or to provide feedback.</p>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Name:
+          <input
+            value={name}
+            name='name'
+            onChange={handleInputChange}
+            onBlur={handleFocusOut}
+            type='text'
+            placeholder='Name'
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            value={email}
+            name='email'
+            onChange={handleInputChange}
+            onBlur={handleFocusOut}
+            type='text'
+            placeholder='Email'
+          />
+        </label>
+        <label>
+          Message:
+          <textarea
+            value={message}
+            name='message'
+            onChange={handleInputChange}
+            onBlur={handleFocusOut}
+            type='text'
+            placeholder='Message'
+            rows={10}
+          />
+        </label>
+        <div className={styles.Footer}>
+          <button 
+            className='Button'
+            type="submit"
+          >
+            Submit
+          </button>
+          <span className={styles.Message}>{errorMessage}</span>
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,9 +3,8 @@ import styles from './Pages.module.css';
 
 const Contact = () => {
     return (
-        <div className={styles.Page}>
+        <div className={styles.PageCol}>
             <h3  className={styles.Header}>Contact</h3>
-            <p className={ styles.Note } style={{ color: 'red', fontWeight: 600, margin: '1rem' }}>Warning: This form is just a placeholder and does not yet function. If you would like to contact me please use one of the links in the footer.</p>
             <ContactForm />
         </div>
     )
